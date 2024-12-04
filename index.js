d1761c7a35bdfeacad5ae54033d769e4f7c6f54e
Himanshi Dixit <hd271198@gmail.com>
2024-12-05 01:07:01 +0530

    messg

--- index.js
+++ index.js
@@ -22,6 +22,6 @@ app.use(express.json())
 app.use('/api',require('./Routes/CreateUser'));
 app.use('/api',require('./Routes/DisplayData'));
 app.use('/api',require('./Routes/orderData'))
-app.listen("https://foodmernappbackend.onrender.com", () => {
+app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })

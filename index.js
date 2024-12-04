6c63030ca597d92ca813ee206df938adfd5ec009
Himanshi Dixit <hd271198@gmail.com>
2024-12-05 00:36:53 +0530

    message

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.use((req,res,next)=>{
-  res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin","https://foodmernappbackend.onrender.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
